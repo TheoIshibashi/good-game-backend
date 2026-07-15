53befcdeee0c28287a796d298b68426fccd35e26
Theo Ishibashi <theoshiba2005@gmail.com>
2026-07-14 23:25:14 -0300

    refactor: return list of results with defensive validation

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from fastapi import FastAPI
-from fastapi import HTTPException
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import requests
 
 app = FastAPI()
 
-app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
@@ -14,34 +12,39 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 def read_root():
     return {"Message": "Meu hub de Games está vivo!"}
 
+
 class GameResponse(BaseModel):
+    id: str
     title: str
     usd_price: float | None
     image: str | None
 
 
-@app.get("/api/games", response_model=GameResponse)
+@app.get("/api/games", response_model=list[GameResponse])
 def read_games(name: str):
     url = f"https://www.cheapshark.com/api/1.0/games?title={name}"
     response = requests.get(url)
 
     game_data = response.json()
+
     if not game_data:
-       raise HTTPException(status_code=404, detail="Game not found.")
-    
-    first_game = game_data[0] 
+        raise HTTPException(status_code=404, detail="Game not found.")
 
-    if not first_game["external"]:
-        raise HTTPException(status_code=502, detail="Title is Missing.")
+    data = []
 
-    cleaned_data = {
-        "title": first_game.get("external"),
-        "usd_price": first_game.get("cheapest", None),
-        "image": first_game.get("thumb", None)
+    for g in game_data:
+        if not g.get("gameID") or not g.get("external"):
+            continue
+        data.append({
+            "id": g.get("gameID"),
+            "title": g.get("external"),
+            "usd_price": float(g["cheapest"]) if g.get("cheapest") else None,
+            "image": g.get("thumb"),
+        })
 
-    }
-    return cleaned_data
+    return data
